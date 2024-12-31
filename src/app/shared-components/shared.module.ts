bf6ced5790e96992f54727398ef60e0a70acdbdb
Maxim handley <mh@Maxims-MacBook-Pro.local>
2024-12-31 01:48:29 +0000

    nicer UI

--- src/app/shared-components/shared.module.ts
+++ src/app/shared-components/shared.module.ts
@@ -4,15 +4,23 @@ import { TileComponent } from '../shared-components/tile/tile.component';
 import { BoardComponent } from '../shared-components/board/board.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     TileComponent,
     BoardComponent,
+    NavbarComponent
   ],
   imports: [BrowserModule, CommonModule],
   providers: [],
-  exports: [TileComponent, BoardComponent, BrowserModule, CommonModule],
+  exports: [
+    TileComponent, 
+    BoardComponent, 
+    BrowserModule, 
+    CommonModule, 
+    NavbarComponent
+],
   bootstrap: [BoardComponent, TileComponent]
 })
 export class SharedModule { }
